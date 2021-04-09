c330d6284a36b48ba077002ab74d5ce543240f65
Alexandre Caramel <caramel.alexandre@gmail.com>
2021-04-09 16:48:43 +0200

    possibiliter de calculer les nombres a virgule

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -15,17 +15,17 @@ class MainActivity : AppCompatActivity() {
     private var input: Float? = null
     private var previousInput: Float? = null
     private var symbol: String? = null
-    private var zero: Boolean? = false
-    private var virgule: Boolean = false
-
-
+    private var zero: Boolean = false
+    private  var point: Boolean = false
+    private var previousPoint: Int = 0
+    private var previousSymbol: Float? = null
     companion object {
         private val INPUT_BUTTONS = listOf(
-                listOf("Ce", "C"),
-                listOf("1", "2", "3", "/"),
-                listOf("4", "5", "6", "*"),
-                listOf("7", "8", "9", "-"),
-                listOf("0", ".", "=", "+")
+            listOf("Ce", "C"),
+            listOf("1", "2", "3", "/"),
+            listOf("4", "5", "6", "*"),
+            listOf("7", "8", "9", "-"),
+            listOf("0", ".", "=", "+")
         )
     }
 
@@ -43,17 +43,17 @@ class MainActivity : AppCompatActivity() {
     private fun addCells(linearLayout: LinearLayout, position: Int) {
         for (x in INPUT_BUTTONS[position].indices) {
             linearLayout.addView(
-                    TextView(
-                            ContextThemeWrapper(this, R.style.CalculatorInputButton)
-                    ).apply {
-                        text = INPUT_BUTTONS[position][x]
-                        setOnClickListener { onCellClicked(this.text.toString()) }
-                    },
-                    LinearLayout.LayoutParams(
-                            0,
-                            ViewGroup.LayoutParams.MATCH_PARENT,
-                            1f
-                    )
+                TextView(
+                    ContextThemeWrapper(this, R.style.CalculatorInputButton)
+                ).apply {
+                    text = INPUT_BUTTONS[position][x]
+                    setOnClickListener { onCellClicked(this.text.toString()) }
+                },
+                LinearLayout.LayoutParams(
+                    0,
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    1f
+                )
             )
         }
     }
@@ -61,133 +61,132 @@ class MainActivity : AppCompatActivity() {
     private fun onCellClicked(value: String) {
         when {
             value.isNum() -> {
-                if(virgule){
-
+                if(point == true){
                     input = (value.toFloat() / 10) + previousInput!!
                     updateDisplayContainer(input.toString())
-                    virgule = false
+                    point = false
+                    previousPoint = 1
+                }else if(previousPoint != 0) {
+                    input = value.toFloat()
+                    updateDisplayContainer( value!!)
+                    previousPoint!! + 1
                 }else{
                     input = value.toFloat()
-                    updateDisplayContainer(value)
+                    updateDisplayContainer(input.toString())
                 }
             }
-            value == "."-> onPointClicked()
-            value == "Ce"-> onCeClicked()
-            value == "C" -> onCClicked()
+            value == "Ce"->{
+                onCeClicked()
+            }
+            value == "C"->{
+                onCclicked()
+            }
+            value == "." -> {
+                onPointClicked()
+            }
             value == "=" -> onEqualsClicked()
             listOf("/", "*", "-", "+").contains(value) -> onSymbolClicked(value)
         }
-
-
     }
-
-    private fun onPointClicked() {
-        updateDisplayContainer(".")
-        virgule = true
-        previousInput = input
-    }
-
-    private fun onCClicked() {
-        if(symbol != null && input != null){
+    private fun onCclicked() {
+        if(point == true){
+            point = false
+            input = previousInput
+            previousInput = null
+            updateDisplayContainer(input.toString())
+        }else if (symbol != null && input != null && previousPoint == 0 ){
             input = null
-            updateDisplayContainer("")
-        }
-        else if(symbol != null && input == null){
+            updateDisplayContainer("symbole")
+        }else if(symbol != null && input != null && previousPoint == 0){
+        }else if(symbol != null && input == null  && previousPoint == 0 ){
             symbol = null
             input = previousInput
             previousInput = null
             updateDisplayContainer(input.toString())
-        }
-        else if(symbol == null && input != null) {
+        }else if(symbol == null && input != null && previousPoint == 0){
             input = null
-            previousInput = null
             symbol = null
+            previousInput = null
             updateDisplayContainer("")
+        }else if(symbol == null && input != null && previousPoint != 0){
+            if(previousPoint ==  1 ){
+                previousPoint!! - 1
+                input = null
+                point = true
+                updateDisplayContainer(".")
+            }else{
+                previousPoint!! - 1
+                input = previousInput
+                previousInput = null
+                updateDisplayContainer(input.toString())
+            }
         }
-
     }
 
-    private fun onCeClicked() {
+    private fun onCeClicked(){
         input = null
         previousInput = null
         symbol = null
         updateDisplayContainer("")
     }
-
     private fun updateDisplayContainer(value: Any) {
         if(zero == true){
             findViewById<TextView>(R.id.calculator_display_container).text = "ERROR"
             zero = false
         }else{
             findViewById<TextView>(R.id.calculator_display_container).text = value.toString()
-
         }
 
     }
-
-    fun String.isNum(): Boolean {
+    private fun onPointClicked(){
+        updateDisplayContainer(".")
+        point = true
+        previousInput = input
+        input = null
+    }
+    private fun String.isNum(): Boolean {
         return length == 1 && isDigitsOnly()
     }
 
     private fun onSymbolClicked(symbol: String) {
         this.symbol = symbol
+        previousSymbol = input
         previousInput = input
         input = null
     }
 
     private fun onEqualsClicked() {
         if (input == null || previousInput == null || symbol == null) {
+
             return
         }
 
         updateDisplayContainer(when (symbol) {
-            "+" -> {
-                input!! + previousInput!!
-
-            }
-            "-" -> {
-
-                previousInput!! - input!!
-            }
-            "*" -> {
-                input!! * previousInput!!
-
-            }
+            "+" ->   input!! +  previousSymbol!!
+            "-" ->  previousSymbol!! - input!!
+            "*" ->  input!! *  previousSymbol!!
             "/" ->
-                if (previousInput == 0.0f || input == 0.0f) {
+                if ( previousSymbol == 0.0f || input == 0.0f){
                     zero = true
-
-                } else {
-                    previousInput!! / input!!
-
+                } else{
+                    previousSymbol!! / input!!
                 }
             else -> "ERROR"
         })
         when (symbol) {
-            "+" -> {
-                input = input!! + previousInput!!
-
-            }
-            "-" -> {
-
-                input = previousInput!! - input!!
-            }
-            "*" -> {
-
-                input = input!! * previousInput!!
-            }
+            "+" -> input =  input!! + previousSymbol!!
+            "-" -> input = previousSymbol!! - input!!
+            "*" -> input = input!! * previousSymbol!!
             "/" ->
-                if (previousInput == 0.0f || input == 0.0f) {
-                    zero = true
-
-                } else {
-
-                    input = previousInput!! / input!!
+                if (previousSymbol == 0.0f || input == 0.0f){
+                    input = input
+                } else{
+                    input =  previousSymbol!! / input!!
                 }
-            else -> "ERROR"
         }
         previousInput = null
         symbol = null
+        previousSymbol = null
     }
 
 }
